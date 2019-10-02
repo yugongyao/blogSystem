7a9a5b22e4c9e48292a982ab261f01e8c8f9cbed
YuGYAO <ygy851934017@qq.com>
2019-10-02 16:46:53 +0800

    git add -A

--- app/static/scripts/api.js
+++ app/static/scripts/api.js
@@ -1,5 +1,6 @@
-var host='http://192.168.0.104:9500';
 
-var homeBannerApi=host+'/api/banner/list';
+  var host='http://192.168.0.104:9500';
 
-var goodsDetailApi=host+'/api/goods/detail';
+  var homeBannerApi=host+'/api/banner/list';
+
+  var goodsDetailApi=host+'/api/goods/detail';
--- app/static/scripts/home.js
+++ app/static/scripts/home.js
@@ -1,4 +1,1 @@
-var h2=document.querySelector('h2');
-h2.onclick=function(){
-  alert('123');
-}
+// var $ = require('jquery');
